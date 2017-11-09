b5c0e95cd00160f8ebdb18381468db14e0d729a6
Dylan Weaver <dylansweaver@gmail.com>
2017-11-09 12:47:13 -0600

    with a twitter Timeline!!!!

--- app/src/main/java/com/example/dweaver/followspace/Homepage.java
+++ app/src/main/java/com/example/dweaver/followspace/Homepage.java
@@ -1,15 +1,19 @@
 package com.example.dweaver.followspace;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.TextView;
+
+import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
+import com.twitter.sdk.android.tweetui.UserTimeline;
 
 /**
  * Created by dweaver on 11/2/17.
  */
 
-public class Homepage extends Activity {
+public class Homepage extends ListActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +22,16 @@ public class Homepage extends Activity {
 
         TextView uname = findViewById(R.id.TV_username);
         uname.setText(username);
+        setContentView(R.layout.homepage);
+
+        final UserTimeline userTimeline = new UserTimeline.Builder()
+                .screenName("spaceX")
+                .build();
+        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
+                .setTimeline(userTimeline)
+                .build();
+        setListAdapter(adapter);
+
+
     }
 }
